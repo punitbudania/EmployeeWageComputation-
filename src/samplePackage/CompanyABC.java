b99d2d791568d6cb5f1fabed243482de9575906c
punitbudania <punitbudania88@gmail.com>
2020-11-22 22:55:56 +0530

    [punit]: added multiple companies UC8

---
+++ src/samplePackage/CompanyABC.java
@@ -0,0 +1,43 @@
+package samplePackage;
+
+public class CompanyABC
+{
+	int EMP_RATE_PER_HOUR = 20;
+    int NUM_OF_WORKING_DAYS = 5;
+    int MAX_HRS_IN_MONTH = 20;
+    final int IS_PART_TIME = 1;
+    final int IS_FULL_TIME = 2;
+    
+    public void wagecompute()
+    {
+    	int totalWage = 0;
+        int totalEmpHrs = 0;
+        int totalWD = 0;
+        //cOMPUTATION
+        while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWD < NUM_OF_WORKING_DAYS)
+        {
+          	int empHrs = 0;
+            int empWage = 0;
+            totalWD++;
+
+            int empCheck = (int) Math.floor(Math.random()*10)%3;
+
+            switch (empCheck)
+            {
+               	case IS_FULL_TIME:
+               		empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalWage += empWage;
+            totalEmpHrs += empHrs;
+        }
+        
+        System.out.println("total Emp wage = " + totalWage);
+    }
+}
--- src/samplePackage/EmployeeWage.java
+++ src/samplePackage/EmployeeWage.java
@@ -1,46 +1,32 @@
 package samplePackage;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class EmployeeWage
 {
 	public static void main(String[] args)
 	{
-		final int IS_PART_TIME = 1;
-	    final int IS_FULL_TIME = 2;
-	    int EMP_RATE_PER_HOUR = 20;
-	    int NUM_OF_WORKING_DAYS = 5;
-	    int MAX_HRS_IN_MONTH = 20;
-	    
-	    
-	    int totalWage = 0;
-	    int totalEmpHrs = 0;
-	    int totalWD = 0;
-	    //cOMPUTATION
-	    while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWD < NUM_OF_WORKING_DAYS)
-	    {
-	      	int empHrs = 0;
-	        int empWage = 0;
-	        totalWD++;
-
-	        int empCheck = (int) Math.floor(Math.random()*10)%3;
-
-	        switch (empCheck)
-	        {
-	           	case IS_FULL_TIME:
-	           		empHrs = 8;
-	                break;
-	            case IS_PART_TIME:
-	                empHrs = 4;
-	                break;
-	            default:
-	                empHrs = 0;
-	        }
-	        empWage = empHrs * EMP_RATE_PER_HOUR;
-	        totalWage += empWage;
-	        totalEmpHrs += empHrs;
-	        System.out.println("Emp Hrs: "+empHrs);
-	        System.out.println("Emp Wage: "+empWage);
-	    }
-	        
-	    System.out.println("total Emp wage = " + totalWage);
+		System.out.println("Select Company:");
+		System.out.println("Press 1 for abc");
+		System.out.println("Press 2 for xyz");
+		
+		Scanner input = new Scanner(System.in);
+		int option = input.nextInt();
+		
+		if(option == 1)
+		{
+			CompanyABC abc = new CompanyABC();
+			abc.wagecompute();
+		}
+		else if(option == 2)
+		{
+			CompanyXYZ xyz =new CompanyXYZ();
+			xyz.wagecompute();
+		}
+		else
+		{
+			System.out.println("invalid selection");
+		}
 	}
 }
